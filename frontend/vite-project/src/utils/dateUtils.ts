ee4a1d3277def0a5a2db667a05678eb8ff7f6e48
fabianjim <fabian.jim26@gmail.com>
2026-04-06 22:17:23 -0400

    Add datetime function for EST conversion for frontend

--- frontend/vite-project/src/utils/dateUtils.ts
+++ frontend/vite-project/src/utils/dateUtils.ts
@@ -5,3 +5,17 @@ export const roundToMinute = (timestamp: string): string => {
     date.setSeconds(0, 0);
     return date.toLocaleTimeString("en-US", {timeZone: "America/New_York", hour: '2-digit', minute: '2-digit'});
 }
+
+// formats timestamp to date and time in EST
+export const formatDateTime = (timestamp: string): string => {
+    if(!timestamp) return '';
+    const date = new Date(timestamp);
+    return date.toLocaleString("en-US", {
+        timeZone: "America/New_York",
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+}
